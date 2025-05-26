3a9de0eaaa1a013ec6c87da35b5d3331ccf722e0
Aniqa Fatima <156777645+aniqafatima203@users.noreply.github.com>
2025-05-26 20:06:32 +0500

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,44 @@
+# Simple Output
+print("Hello World!")
+
+# Variables
+name = "Aniqa"
+age = 21
+price = 29.5
+print("My name is ",name)
+print("My age is ",age)
+print("Price will be ",price)
+
+# DataType
+print(type(name))
+print(type(age))
+print(type(price))
+
+# Arithematic Operator
+a = 2
+b = 3
+print("Sum = ", a + b)
+print("Diff = ", a - b)
+print("Mul = ", a * b)
+print("Div = ", a / b)
+print("Mod = ", a % b)
+print("Power = ", a ** b) #a^b
+
+# Relational/Comparision Operator
+c = 20
+d = 30
+print("c=d :",c==d)
+print("c!=d :",c!=d)
+print("c<=d :",c<=d)
+print("c=>d :",c>=d)
+print("c<d :",c<d)
+print("c>d :",c>d)
+
+# Assignment Operator
+num = 10
+print(num += 5)
+print(num -= 5)
+print(num *= 5)
+print(num /= 5)
+print(num %= 5)
+print(num **= 5)
